33409608989fb11d1836e53355adba03d2ba0da6
Glafira Churilovich <g.churilovich@thomsonreuters.com>
2020-08-05 00:16:07 +0300

    update config

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 import "./Greeting.css";
+import Logo from "../../assets/giphy.gif";
 
-export const Greeting = () => <h1>Hello, world!</h1>;
+export const Greeting = () => (
+  <>
+    <h1>Hello, world!</h1>
+    <img alt="Best greeting ever" src={Logo} />
+  </>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: "./src/index.js",
@@ -16,7 +21,25 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },
@@ -24,8 +47,19 @@ module.exports = {
     extensions: ["*", ".js", ".jsx"],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+    }),
   ],
+  devtool: "eval-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 };
